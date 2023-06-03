80403f960838ca4c0b642e48882ac96e6fa9c19e
unknown <fadl2009gh@gmail.com>
2023-06-03 18:48:54 +0300

    Added Ui winning scene

---
+++ Assets/Script/GameMaster.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour
+{
+    [SerializeField] private GameObject WinMenu;
+    [SerializeField] private GameObject HealthBar;
+    [SerializeField] private PlayerHealth player;
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    void Update()
+    {
+        if (player.health == 20)
+        {
+            PlayerWin();
+        } 
+    }
+
+    public void PlayerWin()
+    {
+        WinMenu.SetActive(true);
+        HealthBar.SetActive(false);
+        Time.timeScale = 0f;
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
--- Assets/Script/Player/PlayerHealth.cs
+++ Assets/Script/Player/PlayerHealth.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int health = 10;
-    
+    public int health = 20;
+    public Slider s;
     public void EditHealth(int amount)
     {
         health += amount;
@@ -14,14 +15,16 @@ public class PlayerHealth : MonoBehaviour
     {
         print(health);
 
-        if (health > 10)
+        if (health > 20)
         {
-            health = 10;
+            health = 20;
         }
 
         if (health < 0)
         {
             health = 0;
         }
+
+        s.value = health;
     }
 }
